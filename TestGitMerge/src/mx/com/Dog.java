9572a389272863cc69df4fe5dacfa626b17c8460
Moises Linares <blade_corp@hotmail.com>
2017-05-20 19:29:48 -0500

    res

--- TestGitMerge/src/mx/com/Dog.java
+++ TestGitMerge/src/mx/com/Dog.java
@@ -1,12 +1,15 @@
 package mx.com;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dog extends Animal{
 
 	public void metodoDog(){
 		List<String> cadenas = new ArrayList<>();
 	}
 	
-	public void metodoConGit(){
+	public void metodoConConflict(){
 	
 	}
 	
